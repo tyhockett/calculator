3a120db1f335e9e7e6ff3338a627ed6a2cf1819c
Ty Hockett <ty@hockett.us>
2019-07-06 17:02:09 -0400

    Adding key bindings

--- calculator-app/src/Calculator/Calculator.js
+++ calculator-app/src/Calculator/Calculator.js
@@ -4,13 +4,22 @@ import styles from "./Calculator.module.scss";
 class Calculator extends PureComponent {
     constructor(props) {
         super(props);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
         this.state = {
             output: "0",
             operator: "",
             previousValue: 0,
             operatorEntered: false
         };
-      }
+    }
+
+    componentDidMount() {
+        document.addEventListener("keypress", this.handleKeyPress);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("keypress", this.handleKeyPress);
+    }
 
     appendValue(value) {
         if (this.state.output === "0" || this.state.operatorEntered) {
@@ -27,7 +36,8 @@ class Calculator extends PureComponent {
 
     clearValue() {
         this.setState({
-            output: "0"
+            output: "0",
+            previousValue: 0,
         });
     }
 
@@ -98,6 +108,74 @@ class Calculator extends PureComponent {
         this.setState({
             output: (this.getCurrentValue() / 100).toString()
         });
+    }
+
+    handleKeyPress(event) {
+        switch (event.keyCode) {
+            default:
+                break;
+
+            case 48:
+                this.appendValue("0");
+                break;
+
+            case 49:
+                this.appendValue("1");
+                break;
+
+            case 50:
+                this.appendValue("2");
+                break;
+
+            case 51:
+                this.appendValue("3");
+                break;
+
+            case 52:
+                this.appendValue("4");
+                break;
+
+            case 53:
+                this.appendValue("5");
+                break;
+
+            case 54:
+                this.appendValue("6");
+                break;
+
+            case 55:
+                this.appendValue("7");
+                break;
+
+            case 56:
+                this.appendValue("8");
+                break;
+
+            case 57:
+                this.appendValue("9");
+                break;
+
+            case 42:
+                this.setOperator("*");
+                break;
+
+            case 43:
+                this.setOperator("+");
+                break;
+
+            case 45:
+                this.setOperator("-");
+                break;
+
+            case 47:
+                this.setOperator("/");
+                break;
+
+            case 13:
+            case 61:
+                this.solve();
+                break;
+        }
     }
 
     render() {
